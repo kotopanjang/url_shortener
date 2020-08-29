23b746898bcfd9758cac5f2a102cd49bd0676525
kotopanjang <rahadian.ardya@gmail.com>
2020-08-29 21:24:25 +0700

    Add files via upload

---
+++ routes/routes.go
@@ -0,0 +1,19 @@
+package routes
+
+import (
+	"aqilliz_assesment/controllers"
+
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+	router := gin.Default()
+
+	router.Use(static.Serve("/", static.LocalFile("./views", true)))
+	router.GET("/register", controllers.Register)
+	router.GET("/retrieve", controllers.Retrieve)
+	router.GET("/redirect", controllers.Redirect)
+
+	return router
+}
